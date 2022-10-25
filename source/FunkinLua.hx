976741c3d2f3c79003d71b2fd3ad03f77e65342f
LEVIXDDLMAO <levi_xddd@outlook.com>
2022-10-25 04:24:16 +0000

    let go

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -118,7 +118,7 @@ class FunkinLua {
 		set('rating', 0);
 		set('ratingName', '');
 		set('ratingFC', '');
-		set('version', MainMenuState.psychEngineVersion.trim());
+		set('version', MainMenuState.infEngineVersion.trim());
 		
 		set('inGameOver', false);
 		set('mustHitSection', false);
--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -25,7 +25,7 @@ class OutdatedState extends MusicBeatState
 
 		warnText = new FlxText(0, 0, FlxG.width,
 			"Sup bro, looks like you're running an   \n
-			outdated version of Psych Engine (" + MainMenuState.psychEngineVersion + "),\n
+			outdated version of Infinity Engine (" + MainMenuState.infEngineVersion + "),\n
 			please update to " + TitleState.updateVersion + "!\n
 			\n
 			Thank you for using the Engine!",
@@ -40,7 +40,7 @@ class OutdatedState extends MusicBeatState
 		if(!leftState) {
 			if (controls.ACCEPT || controls.BACK) {
 				leftState = true;
-				CoolUtil.browserLoad("https://github.com/ShadowMario/FNF-PsychEngine/releases");
+				CoolUtil.browserLoad("https://github.com/LEVIXDDLMAO/FNF-Infinity-Engine/releases");
 				FlxG.sound.play(Paths.sound('cancelMenu'));
 				FlxTween.tween(warnText, {alpha: 0}, 1, {
 					onComplete: function (twn:FlxTween) {
