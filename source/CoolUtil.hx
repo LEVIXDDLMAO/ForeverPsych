93d61bdf82ad30d1d63cb7faa787c215e75e184a
LEVIXDDLMAO <levi_xddd@outlook.com>
2022-10-29 21:10:50 +0000

    your mom

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -19,7 +19,9 @@ class CoolUtil
 	public static var defaultDifficulties:Array<String> = [
 		'Easy',
 		'Normal',
-		'Hard'
+		'Hard',
+		'Hard+',
+		'Erect'
 	];
 	public static var defaultDifficulty:String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
